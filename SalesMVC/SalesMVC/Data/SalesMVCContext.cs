b5e7fc2043429648efc69ad7fe947991d9417874
breno <brenofranciscomorais@gmail.com>
2020-09-28 05:01:51 -0400

    -Alinhando-

---
+++ SalesMVC/SalesMVC/Data/SalesMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMVC.Models;
+
+namespace SalesMVC.Data
+{
+    public class SalesMVCContext : DbContext
+    {
+        public SalesMVCContext (DbContextOptions<SalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesMVC.Models.Department> Department { get; set; }
+    }
+}
---
+++ SalesMVC/SalesMVC/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesMVC.Models
+{
+    //Departamento 
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string data { get; set; }
+    }
+}
